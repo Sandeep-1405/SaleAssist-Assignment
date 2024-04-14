66af105a839a41bbeca027436747636dafbf125e
Sandeep-1405 <sandeepmaraboina14s@gmail.com>
2024-04-14 14:31:17 +0530

    assignment

---
+++ frontend/src/components/Home/home.js
@@ -0,0 +1,173 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import './home.css'
+
+function Home(){
+    return(
+        <div className="bg">
+
+            <div className='min-nav'>
+                <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713028050/TANN_TRIM_sig6nb.png" className="text-light m-3"/>
+
+                <div>
+
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713028414/Frame_51_wqjusd.png" className="m-3"/>
+
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713028176/Frame_52_oaw3oi.png" className="m-3"/>
+
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713028248/Frame_53_ipprwh.png" className="m-3"/>
+
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713028298/Frame_54_ckevag.png" className="m-3"/>
+                </div>
+            </div>
+
+            <div className="text-light d-flex flex-row justify-content-center flex-wrap ">
+                <p className="m-3 para">Bags</p>
+                <p className="m-3">Travel</p>
+                <p className="m-3">Accesories</p>
+                <p className="m-3">Gifting</p>
+                <p className="m-3">Jewelery</p>
+            </div>
+            
+            <div className="d-flex flex-row justify-content-between flex-wrap">
+
+                <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713026313/Group_475_ctavjz.png" alt="" className="p-3"/>
+
+                <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713026581/Group_307_unptll.png" className="p-3"/>
+
+                <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713026632/Group_300_blc0z4.png" className="p-3"/>
+
+                <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713026658/Group_301_fhf5v1.png" className="p-3"/>
+
+                <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713026685/Group_302_mksyc9.png" className="p-3"/>
+
+                <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713026722/Group_303_uehjdu.png" className="p-3"/>
+
+                <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713026750/Group_304_ytihnj.png" className="p-3"/>
+
+                <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713026795/Group_305_rmovqa.png" className="p-3"/>
+
+                <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713026816/Group_306_siv5w9.png" className="p-3"/>
+
+            </div>
+
+            <div className="d-flex flex-row justify-content-between text-light m-3 flex-wrap">
+                <p className="m-3 para">Bags . Backpacks</p>
+
+                <div className="d-flex">
+                    <p className="m-3 para">13 products</p>
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713029105/Vector_4_buulsm.png" className="m-3"/>
+                </div>
+
+            </div>
+
+            <div className="d-flex flex-row justify-content-around flex-wrap text-light">
+
+                <div className="cards bg-dark m-3">
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030770/i1_vcytpg.png" />
+                    <h6>The Brown Metro Movers</h6>
+                    <div className="d-flex flex-row justify-content-between m-1">
+                        <div className="d-flex mt-3">
+                            <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030248/rupee-sign_uz6iny.png" className="pr-2 rupee"/>
+                            <p className="price para">4899 <del>8999</del> <span>(50% Off)</span></p>
+                        </div>
+
+                        <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030462/Group_450_u9sxo1.png" className="m-2"/>
+                    </div>
+                </div>
+                
+                <div className="cards bg-dark m-3">
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713032426/i2_spgyym.png" />
+                    <h6>The Metro Movers Black</h6>
+                    <div className="d-flex flex-row justify-content-between m-1">
+                        <div className="d-flex mt-3">
+                            <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030248/rupee-sign_uz6iny.png" className="mr-2 rupee"/>
+                            <p className="price para">4899 <del>8999</del> <span>(50% Off)</span></p>
+                        </div>
+
+                        <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030462/Group_450_u9sxo1.png" className="m-2"/>
+                    </div>
+                </div>
+
+                <div className="cards bg-dark m-3">
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713032477/i3_gduwsm.png" />
+                    <h6>The Metro Movers Black</h6>
+                    <div className="d-flex flex-row justify-content-between m-1">
+                        <div className="d-flex mt-3">
+                            <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030248/rupee-sign_uz6iny.png" className="mr-2 rupee"/>
+                            <p className="price para">4899 <del>8999</del> <span>(50% Off)</span></p>
+                        </div>
+
+                        <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030462/Group_450_u9sxo1.png" className="m-2"/>
+                    </div>
+                </div>
+
+                <div className="cards bg-dark m-3">
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713032509/i4_bodjx2.png" />
+                    <h6>The Metro Movers Black</h6>
+                    <div className="d-flex flex-row justify-content-between m-1">
+                        <div className="d-flex mt-3">
+                            <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030248/rupee-sign_uz6iny.png" className="mr-2 rupee"/>
+                            <p className="price para">4899 <del>8999</del> <span>(50% Off)</span></p>
+                        </div>
+
+                        <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030462/Group_450_u9sxo1.png" className="m-2"/>
+                    </div>
+                </div>
+
+                <div className="cards bg-dark m-3">
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030770/i1_vcytpg.png" />
+                    <h6>The Brown Metro Movers</h6>
+                    <div className="d-flex flex-row justify-content-between m-1">
+                        <div className="d-flex mt-3">
+                            <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030248/rupee-sign_uz6iny.png" className="pr-2 rupee"/>
+                            <p className="price para">4899 <del>8999</del> <span>(50% Off)</span></p>
+                        </div>
+
+                        <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030462/Group_450_u9sxo1.png" className="m-2"/>
+                    </div>
+                </div>
+                
+                <div className="cards bg-dark m-3">
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713032426/i2_spgyym.png" />
+                    <h6>The Metro Movers Black</h6>
+                    <div className="d-flex flex-row justify-content-between m-1">
+                        <div className="d-flex mt-3">
+                            <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030248/rupee-sign_uz6iny.png" className="mr-2 rupee"/>
+                            <p className="price para">4899 <del>8999</del> <span>(50% Off)</span></p>
+                        </div>
+
+                        <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030462/Group_450_u9sxo1.png" className="m-2"/>
+                    </div>
+                </div>
+
+                <div className="cards bg-dark m-3">
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713032477/i3_gduwsm.png" />
+                    <h6>The Metro Movers Black</h6>
+                    <div className="d-flex flex-row justify-content-between m-1">
+                        <div className="d-flex mt-3">
+                            <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030248/rupee-sign_uz6iny.png" className="mr-2 rupee"/>
+                            <p className="price para">4899 <del>8999</del> <span>(50% Off)</span></p>
+                        </div>
+
+                        <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030462/Group_450_u9sxo1.png" className="m-2"/>
+                    </div>
+                </div>
+
+                <div className="cards bg-dark m-3">
+                    <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713032509/i4_bodjx2.png" />
+                    <h6>The Metro Movers Black</h6>
+                    <div className="d-flex flex-row justify-content-between m-1">
+                        <div className="d-flex mt-3">
+                            <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030248/rupee-sign_uz6iny.png" className="mr-2 rupee"/>
+                            <p className="price para">4899 <del>8999</del> <span>(50% Off)</span></p>
+                        </div>
+
+                        <img src="https://res.cloudinary.com/dvip7hkns/image/upload/v1713030462/Group_450_u9sxo1.png" className="m-2"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
